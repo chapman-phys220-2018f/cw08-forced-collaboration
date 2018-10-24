408e00a75c069608ab9f65131f9ea69a1018765f
Conner Carnahan <ConnerCarnahan@gmail.com>
2018-10-24 05:56:08 +0000

    Made test less harsh

--- test_sinesum.py
+++ test_sinesum.py
@@ -9,13 +9,13 @@ import sinesum as ss
 import numpy.testing as npt
 
 def test_f():
-    npt.assert_approx_equal(ss.f(2,0), 0.0, significant=5)
-    npt.assert_approx_equal(ss.f(2,0.5), 1.0, significant = 5)
-    npt.assert_approx_equal(ss.f(2,-0.5), -1.0, significant = 5)
+    npt.assert_approx_equal(ss.f(2,0), 0.0, significant=2)
+    npt.assert_approx_equal(ss.f(2,0.5), 1.0, significant = 2)
+    npt.assert_approx_equal(ss.f(2,-0.5), -1.0, significant = 2)
 
 def test_Sn():
-    npt.assert_approx_equal(ss.Sn(2,0.5,100),1, significant = 5)
-    npt.assert_approx_equal(ss.Sn(2,-0.5,100),-1, significant = 5)
-    npt.assert_approx_equal(ss.Sn(2,0,100), 0 , significant = 5)
+    npt.assert_approx_equal(ss.Sn(2,0.5,100),1, significant = 2)
+    npt.assert_approx_equal(ss.Sn(2,-0.5,100),-1, significant = 2)
+    npt.assert_approx_equal(ss.Sn(2,0,100), 0 , significant = 2)
     
     
